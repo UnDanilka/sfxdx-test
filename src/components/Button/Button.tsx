27c082a208e75d0ce90eca97ac5cf762dad4ee4a
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-03-01 14:15:47 +0700

    feat:added basic header

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Button = () => {
+  return (
+    <div className="button">
+      <div className="button_label">Connect Wallet</div>
+    </div>
+  )
+}
+
+export default Button
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react"
+import Button from "../Button/Button"
+import Logo from "../Logo/Logo"
 
 const Header = () => {
-  return <div className="header"></div>
+  return (
+    <div className="header">
+      <Logo />
+      <Button />
+    </div>
+  )
 }
 
 export default Header
