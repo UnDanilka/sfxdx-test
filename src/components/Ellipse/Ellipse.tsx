53435171490a3015708356c4f664efaf5ca0a491
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-03-01 17:34:11 +0700

    feat: added basic animation

--- src/components/Ellipse/Ellipse.tsx
+++ src/components/Ellipse/Ellipse.tsx
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 const Ellipse = (props: any) => {
-  return <div className="ellipse" style={props} />
+  const [position, setPosition] = useState({})
+
+  useEffect(() => {
+    setPosition({
+      left: Math.floor(Math.random() * window.screen.width),
+      top: Math.floor(Math.random() * window.screen.height),
+    })
+
+    setInterval(() => {
+      setPosition({
+        left: Math.floor(Math.random() * window.screen.width),
+        top: Math.floor(Math.random() * window.screen.height),
+      })
+    }, 5000)
+  }, [])
+
+  return <div className="ellipse" style={{ ...props, ...position }} />
 }
 
 export default Ellipse
