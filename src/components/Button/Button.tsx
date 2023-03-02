0c761ce79739e1d295cb1481faac040feaf38a46
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-03-02 16:34:34 +0700

    feat:added web3js

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,43 @@
+import { useEffect } from "react"
 import { IButtonProps } from "../../types"
 
 const Button = ({ position, width, height, fontSize }: IButtonProps) => {
+  // const { ethereum } = window
+
+  // const connectWallet = async () => {
+  //   try {
+  //     if (!ethereum) {
+  //       return console.log("error", "Wallet is not installed")
+  //     } else {
+  //       const address = await ethereum.request({
+  //         method: "eth_requestAccounts",
+  //       })
+  //       return address[0]
+  //     }
+  //   } catch (e) {
+  //     console.log("error", "Error with wallet connecting")
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const { ethereum } = window
+  //   const showData = (e: any) => console.log(e)
+  //   ethereum.on("accountsChanged", showData)
+  //   ethereum.on("chainChanged", showData)
+  //   return () => {
+  //     ethereum.removeListener("accountsChanged", showData)
+  //     ethereum.removeListener("chainChanged", showData)
+  //   }
+  // }, [])
+
+  //ethereum
+
   return (
-    <div className="button" style={{ position, width, height, fontSize }}>
+    <div
+      className="button"
+      style={{ position, width, height, fontSize }}
+      // onClick={connectWallet}
+    >
       <div className="button_label">Connect Wallet</div>
     </div>
   )
